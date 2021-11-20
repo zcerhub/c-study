ca5253f7490d5bf358d15457c3d59ac51dcf183f
zhangce <zc19920709>
2021-11-20 10:49:57 +0800

    end

---
+++ typeConvertDemo2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int  i = 17;
+   char c = 'c'; /* ascii 值是 99 */
+   int sum;
+ 
+   sum = i + c;
+   printf("Value of sum : %d\n", sum );
+ 
+}
---
+++ typeConvertDemo3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int  i = 17;
+   char c = 'c'; /* ascii 值是 99 */
+   float sum;
+ 
+   sum = i + c;
+   printf("Value of sum : %f\n", sum );
+ 
+}
